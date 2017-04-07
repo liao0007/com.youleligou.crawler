625911359bdd85812c00b648e9643f9214ecdf9f
liaoliang <liao0007@gmail.com>
2017-04-07 12:53:37 +0800

    cleanup

--- src/main/scala/com/youleligou/Main.scala
+++ src/main/scala/com/youleligou/Main.scala
@@ -39,7 +39,7 @@ object Main extends App {
 
   import net.codingwell.scalaguice.InjectorExtensions._
 
-  injector.instance[ProxyAssistantBootstrap].start(FiniteDuration(3, MILLISECONDS))
+  injector.instance[ProxyAssistantBootstrap].start(FiniteDuration(1, MILLISECONDS))
 
   //min interval: 2000/100 = 20 milliseconds
 //  injector.instance[ElemeCrawlerBootstrap].start(FiniteDuration(5, SECONDS), FiniteDuration(1000, MILLISECONDS))
