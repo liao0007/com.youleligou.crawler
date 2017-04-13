0754c8f3898cb7527df4e0293d3e86891dd80051
liaoliang <liao0007@gmail.com>
2017-04-13 17:44:42 +0800

    cleanup

--- src/main/scala/com/youleligou/Main.scala
+++ src/main/scala/com/youleligou/Main.scala
@@ -49,7 +49,7 @@ object Main extends App {
   if (args.contains("eleme/food"))
     injector.instance[ElemeCrawlerBootstrap].startFood()
 
-  if (args.contains("proxy"))
+  if (args.contains("proxy/assistant"))
     injector.instance[ProxyReplenishmentAssistantBootstrap].start()
 
   if (args.contains("proxy/you"))
